f8281d0140e6a9fa9c215f6aa77abbdd3042b7f2
Luke McKechnie <lmckechnie@enova.com>
2020-08-04 15:42:36 -0500

    Bugfixes and ui improvements.

--- public/js/widgets.js
+++ public/js/widgets.js
@@ -1,7 +1,20 @@
 // Basic variable declaration - keep track of how many of each
 // item we currently own, and how much the new ones should cost.
+function chargeForType(type,cost){
+    switch(type){
+        case 'widget':
+            data.widgetCount -= cost;
+            break;
+        case 'dodad':
+            data.dodadCount -= cost;
+            break;
+        default:
+            break;
+    }
+}
 class Worker{
-    constructor(count, speed, purchased, setBonus, setSize, cost, costIncrease){
+    constructor(type,count, speed, purchased, setBonus, setSize, cost, costIncrease, interval){
+        this.type=type;
         this.count = count;
         this.speed = speed;
         this.purchased = purchased;
@@ -9,22 +22,82 @@ class Worker{
         this.setSize = setSize;
         this.cost = cost;
         this.costIncrease = costIncrease;
+        this.interval = interval;
     }
 
+    get countToSet() {
+        return this.setSize - this.purchased%this.setSize;
+    }
     get priceToSet() {
-        numToSet = count % this.setSize;
-        priceToSet = 0;
-        currentPrice = this.cost;
-        for (i=0; i < numToSet; i++){
-            priceToSet+=currentPrice;
+        return this.priceToCount(this.countToSet);
+    }
+    
+    purchase(count){
+        if(count <= 0 || count > this.setSize){
+            return;
+        }
+        this.count += count;
+        this.purchased += count;
+
+        // Deduct cost
+        chargeForType(this.type, this.priceToCount(count));
+        // Increase cost for the next one, using Math.ceil() to round up
+        this.cost = increasePrice(count,this.cost,this.costIncrease);
+    }
+
+    purchaseNextSet(){
+        this.purchase(this.countToSet);
+    }
+
+    priceToCount(count){
+        var priceToCount = 0;
+        var currentPrice = this.cost;
+        for (var i=0; i < count; i++){
+            priceToCount+=currentPrice;
             currentPrice*=this.costIncrease;
         }
-        return numberForDisplay(priceToSet,2);
+        return priceToCount;
+    }
+
+    produce(msSinceUpdate){
+        if (this.count == 0){
+            return 0;
+        }
+        return this.totalSpeed * msSinceUpdate / this.interval ;
+    }
+
+    get totalSpeed(){
+        // get the flat speed rate for count
+        var flatSpeed = this.count * this.speed;
+        //bonus based on how many set groups have been purchased
+        var bonusCount = Math.floor(this.purchased/this.setSize);
+        if (bonusCount == 0) return flatSpeed;
+        return flatSpeed * bonusCount * this.setBonus;
     }
 }
-let data = {
-    widgeteers: {
+class dataClass{
+    constructor(widgeteers, dodadeers, numWidgets, numDodads){
+        this.widgeteers = widgeteers;
+        this.dodadeers = dodadeers;
+        this.widgetCount = numWidgets;
+        this.dodadCount = numDodads;
+    }
+
+    set numWidgets(value){
+        if(isNaN(value)){
+            value = 0;
+        }
+        this.widgetCount = parseFloat(value);
+    }
+
+    get numWidgets(){
+        return this.widgetCount;
+    }
+}
+let data = new dataClass(
+    { //widgeteers
         novice: new Worker(
+            'widget', // type
             0, //count
             1, //speed
             0, //purchased
@@ -32,17 +105,21 @@ let data = {
             10, // setSize
             100, //cost
             1.1, //costIncrease
+            1000, // interval
         ),
         master: new Worker (
+            'widget', // type
             0, //count
             1.1, //speed
             0, //purchased
             1.2, // setBonus
             10, // setSize
-            250, //cost
+            500, //cost
             1.1, //costIncrease
+            1000, // interval
         ),
         expert: new Worker(
+            'widget', // type
             0, //count
             1.1, //speed
             0, //purchased
@@ -50,11 +127,11 @@ let data = {
             10, // setSize
             2500, //cost
             1.1, //costIncrease
+            1000, // interval
         ),
         speed: 0,
-        interval: 100,
     },
-    dodadeers: {
+    { //dodadeers
         numNovices: 0,
         noviceSpeed: 1,
         numNovicesPurchased: 0,
@@ -71,14 +148,17 @@ let data = {
         costIncrease: 1.1,
         interval: 1000,
     },
-    numWidgets: 0,
-    numDodads: 0,
+    0, //numWidgets
+    0, //numDodads
+)
 
-}
-
+var lastRun = Date.now();
+var widgetsLastRun = 0;
+var speedLastRuns = [];
 
 $(document).ready(function(){
-    $('#novice-widgeteer-10').text([10, data.widgeteers.novice.priceToSet].join(' - '));
+    $('.tabs').tabs();
+    $('#novice-widgeteer-10').text([10, numberForDisplay(data.widgeteers.novice.priceToSet,2)].join(' - '));
     
     // Increase numWidgets every time produce-widget is clicked
     $('#produce-widget').off("click").on('click', function () {
@@ -92,36 +172,19 @@ $(document).ready(function(){
 
     // Same for novice-widgeteer
     $('#novice-widgeteer-1').off("click").on('click', function () {
-        data.widgeteers.numNovices++;
-        numNoviceWidgeteersPurchased++;
-        // update the ui with how many we own.
-        $('#novice-widgeteer-count').text(Math.round(numNoviceWidgeteers));
-        
-        // Deduct cost
-        numWidgets -= noviceWidgeteerCost;
-        // Increase cost for the next one, using Math.ceil() to round up
-        noviceWidgeteerCost = Math.ceil(increasePrice(1,noviceWidgeteerCost,widgeteerCostIncrease));
-        // update the buy to 10 button
-        numTo10 = 10-(numNoviceWidgeteersPurchased%10)
-        noviceWidgeteerPriceTo10 = calculatePriceTo10(numTo10, noviceWidgeteerCost, widgeteerCostIncrease);
-        $('#novice-widgeteer-10').text([numTo10, numberForDisplay(noviceWidgeteerPriceTo10,2)].join(' - ')); 
-        // Set the speed of the widgeteers for display
-        widgeteerSpeed = widgeteerSpeed + noviceWidgeteerSpeed;
-        if (numTo10 == 0){
-            widgeteerSpeed = widgeteerSpeed * noviceWidgeteer10Bonus;
-        }
+        data.widgeteers.novice.purchase(1);
+
+        // update the ui with new pricing.
+        $('#novice-widgeteer-1').text('1 - ' + numberForDisplay(data.widgeteers.novice.cost,2));
+        $('#novice-widgeteer-10').text([data.widgeteers.novice.countToSet, numberForDisplay(data.widgeteers.novice.priceToSet,2)].join(' - ')); 
     });
 
     $('#novice-widgeteer-10').off("click").on("click", function(){
-        numTo10 = 10-(numNoviceWidgeteersPurchased%10);
-        numNoviceWidgeteers+= numTo10;
-        numNoviceWidgeteersPurchased += numTo10;
-        noviceWidgeteerPriceTo10 = calculatePriceTo10(numTo10, noviceWidgeteerCost, widgeteerCostIncrease);
-        numWidgets -= noviceWidgeteerPriceTo10;
-        widgeteerSpeed = (widgeteerSpeed + (numTo10*noviceWidgeteerSpeed))*noviceWidgeteer10Bonus;
-        noviceWidgeteerCost = Math.ceil(increasePrice(numTo10, noviceWidgeteerCost, widgeteerCostIncrease));
-        noviceWidgeteerPriceTo10 = calculatePriceTo10(numTo10, noviceWidgeteerCost, widgeteerCostIncrease);
-        $('#novice-widgeteer-10').text([numTo10, numberForDisplay(noviceWidgeteerPriceTo10,2)].join(' - ')); 
+        data.widgeteers.novice.purchaseNextSet();
+        
+        // update the ui with new pricing.
+        $('#novice-widgeteer-1').text('1 - ' + numberForDisplay(data.widgeteers.novice.cost,2));
+        $('#novice-widgeteer-10').text([data.widgeteers.novice.countToSet, numberForDisplay(data.widgeteers.novice.priceToSet,2)].join(' - ')); 
     });
 
     // Same for novice-widgeteer
@@ -139,9 +202,8 @@ $(document).ready(function(){
 
     // Ditto for master-widgeteer... you get the idea
     $('#master-widgeteer-1').off("click").on('click', function () {
-        numMasterWidgeteers++;
-        numWidgets -= masterWidgeteerCost;
-        masterWidgeteerCost = Math.ceil(masterWidgeteerCost * widgeteerCostIncrease);
+        data.widgeteers.master.purchase(1);
+        $('#master-widgeteer-1').text('1 - ' + numberForDisplay(data.widgeteers.master.cost,2));
     });
 
     $('#master-dodadeer').off("click").on("click", function(){
@@ -151,9 +213,8 @@ $(document).ready(function(){
     })
 
     $('#expert-widgeteer-1').off("click").on('click', function () {
-        numExpertWidgeteers++;
-        numWidgets -= expertWidgeteerCost;
-        expertWidgeteerCost = Math.ceil(expertWidgeteerCost * widgeteerCostIncrease);
+        data.widgeteers.expert.purchase(1);
+        $('#expert-widgeteer-1').text('1 - ' + numberForDisplay(data.widgeteers.expert.cost,2));
     });
 
     $('#expert-dodadeer').off("click").on("click", function(){
@@ -165,75 +226,68 @@ $(document).ready(function(){
 
     // Run UI update code every 10ms
     window.setInterval(function () {
-        
+        // since this doesn't actually run at the interval, get how many ms it has been since our last pass
+        let sinceLastRun = Date.now()-lastRun;
+        let widgetSinceLastRun = data.numWidgets - widgetsLastRun;
+        let speedThisRun = widgetSinceLastRun/sinceLastRun * 1000;
+        if(speedThisRun > 0) {
+            if (speedLastRuns.length >10){
+                speedLastRuns.shift();
+            }
+            speedLastRuns.push(speedThisRun);
+        }
+        let average = (array) => array.reduce((a, b) => a+b) / array.length;
+        let averageSpeed = Math.round(average(speedLastRuns));
+
+        widgetsLastRun = data.numWidgets;
+        lastRun = Date.now();
 
         // Masters train 5 Novice Widgeteers per second (5/100 every 10ms)
-        numNoviceWidgeteers += (numMasterWidgeteers * masterWidgeteerSpeed / widgeteerInterval);
+        data.widgeteers.novice.count += data.widgeteers.master.produce(sinceLastRun);
         // Experts train 25 Master Widgeteers per second (25/100 every 10ms)
-        numMasterWidgeteers += (numExpertWidgeteers * expertWidgeteerSpeed / widgeteerInterval);
+        data.widgeteers.master.count += data.widgeteers.expert.produce(sinceLastRun);
         // Novices add 1 per second (1/100 every 10ms)
-        numWidgets += (numNoviceWidgeteers * noviceWidgeteerSpeed / widgeteerInterval);
+        data.numWidgets += data.widgeteers.novice.produce(sinceLastRun);
 
-        $('#novice-widgeteer-count').text(numberForDisplay(numNoviceWidgeteers));
-        $('#master-widgeteer-count').text(numberForDisplay(numMasterWidgeteers));
-        $('#expert-widgeteer-count').text(numberForDisplay(numExpertWidgeteers));
+        $('#novice-widgeteer-count').text(numberForDisplay(data.widgeteers.novice.count));
+        $('#master-widgeteer-count').text(numberForDisplay(data.widgeteers.master.count));
+        $('#expert-widgeteer-count').text(numberForDisplay(data.widgeteers.expert.count));
 
-        // Novices add .1 per second (1/1000 every 10ms)
-        numDodads += (numNoviceDodadeers * noviceDodadeerSpeed / dodadeerInterval);
-
-        // Masters add .5 per second (5/1000 every 10ms)
-        numDodads += (numMasterDodadeers * masterDodadeerSpeed / dodadeerInterval);
-
-        numDodads += (numExpertDodadeers * expertDodadeerSpeed / dodadeerInterval)
 
         // Update the text showing how many widgets we have, using Math.floor() to round down
-        $('#widget-count').text(numberForDisplay(Math.floor(numWidgets),2));
-
-        // Update the text showing how many widgets we have, using Math.floor() to round down
-        $('#dodad-count').text(Math.floor(numDodads));
+        $('#widget-count').text(numberForDisplay(Math.floor(data.numWidgets),2));
 
         // Update the text showing the speed of the widgeteers, rounding down.
-        widgeteerSpeed = numNoviceWidgeteers * noviceWidgeteerSpeed;
-        $('#widgeteer-speed').text(numberForDisplay(Math.floor(widgeteerSpeed),2));
-        $('#novice-widgeteer-speed').text(numberForDisplay(numNoviceWidgeteers * noviceWidgeteerSpeed,2));
-        $('#master-widgeteer-speed').text(numberForDisplay(numMasterWidgeteers * masterWidgeteerSpeed,2));
-        $('#expert-widgeteer-speed').text(numberForDisplay(numExpertWidgeteers * expertWidgeteerSpeed,2));
+        data.widgeteers.speed = averageSpeed; // I'm not sure if we need both, but here we are//
+        $('#widgeteer-speed').text(numberForDisplay(Math.floor(data.widgeteers.speed),2));
+        $('#novice-widgeteer-speed').text(numberForDisplay(data.widgeteers.novice.totalSpeed,2));
+        $('#master-widgeteer-speed').text(numberForDisplay(data.widgeteers.master.totalSpeed,2));
+        $('#expert-widgeteer-speed').text(numberForDisplay(data.widgeteers.expert.totalSpeed,2));
 
-        dodadeerSpeed = numNoviceDodadeers * noviceDodadeerSpeed;
-        $('#dodadeer-speed').text(numberForDisplay(dodadeerSpeed,2));
-        $('#novice-dodadeer-speed').text(numNoviceDodadeers * noviceDodadeerSpeed);
-        $('#master-dodadeer-speed').text(numMasterDodadeers * masterDodadeerSpeed);
-        $('#expert-dodadeer-speed').text(numExpertDodadeers * expertDodadeerSpeed);
 
         // Update the widgeteers with their current prices
-        $('#novice-widgeteer-1').text('1 - ' + numberForDisplay(noviceWidgeteerCost,2));
-        $('#master-widgeteer-1').text('1 - ' + numberForDisplay(masterWidgeteerCost,2));
-        $('#expert-widgeteer-1').text('1 - ' + numberForDisplay(expertWidgeteerCost,2));
+        
+        
 
         // Enable/disable the widgeteer buttons based on our numWidgets
-        $('#novice-widgeteer-1').prop('disabled', noviceWidgeteerCost > numWidgets);
-        $('#novice-widgeteer-10').prop('disabled', noviceWidgeteerPriceTo10 > numWidgets )
-        $('#master-widgeteer-1').prop('disabled', masterWidgeteerCost > numWidgets || numNoviceWidgeteers < 10);
-        $('#expert-widgeteer-1').prop('disabled', expertWidgeteerCost > numWidgets || numMasterWidgeteers < 10);
+        $('#novice-widgeteer-1').prop('disabled', data.widgeteers.novice.cost > data.numWidgets);
+        $('#novice-widgeteer-10').prop('disabled', data.widgeteers.novice.priceToSet > data.numWidgets )
+        $('#master-widgeteer-1').prop('disabled', data.widgeteers.master.cost > data.numWidgets || data.widgeteers.novice.count < 10);
+        $('#expert-widgeteer-1').prop('disabled', data.widgeteers.expert.cost > data.numWidgets || data.widgeteers.master.count < 10);
 
-        // Update the widgeteers with their current prices
-        $('#novice-dodadeer').text('Hire Novice Dodadeer - ' + noviceDodadeerCost);
-        $('#master-dodadeer').text('Hire Master Dodadeer - ' + masterDodadeerCost);
-        $('#expert-dodadeer').text('Hire Expert Dodadeer - ' + expertDodadeerCost);
-
-        // Enable/disable the widgeteer buttons based on our numWidgets
-        $('#novice-dodadeer').prop('disabled', noviceDodadeerCost > numDodads);
-        $('#master-dodadeer').prop('disabled', masterDodadeerCost > numDodads);
-        $('#expert-dodadeer').prop('disabled', expertDodadeerCost > numDodads);
-    }, 10);
+    }, 100);
 
 
 })
 
 function numberForDisplay(number, numDecimal = 0){
     // TODO: Create an option to change this to other types. Maybe include a cancer notation IE Antimatter?
+    if(isNaN(number)){
+        //try parsing it to a float
+        number=parseFloat(number);
+    }
     if (number < 10000){
-        return number.toFixed(numDecimal);
+        return number.toFixed(0);
     }
     return number.toExponential(numDecimal);
 }
